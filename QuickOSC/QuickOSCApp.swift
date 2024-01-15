62996ca14a880affbb34fd3ad7c22ae0b0ee7015
flvffywvffy <flvffywvffy@gmail.com>
2024-01-15 23:44:16 +0000

    Initial Commit

---
+++ QuickOSC/QuickOSCApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickOSCApp.swift
+//  QuickOSC
+//
+//  Created by Marcus Harvey on 15/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuickOSCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
