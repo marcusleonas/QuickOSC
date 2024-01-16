80255464a5446c9a5b3b678e5d14e9a754c388e7
flvffywvffy <flvffywvffy@gmail.com>
2024-01-16 03:14:06 +0000

    Finished bulk of app, ready for beta release

---
+++ QuickOSC/ApplicationMenu.swift
@@ -0,0 +1,18 @@
+//
+//  ApplicationMenu.swift
+//  QuickOSC
+//
+//  Created by Marcus Harvey on 16/01/2024.
+//
+
+import SwiftUI
+
+struct ApplicationMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApplicationMenu()
+}
